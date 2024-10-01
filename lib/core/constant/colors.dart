9f94b13da4b1c4f6c973de9659d6bf45cdc39d6d
danielpinheirohs <danielpinheirohs@gmail.com>
2024-10-01 07:36:31 -0300

    Adicionado as cores padronizadas.

---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const Color blue = Colors.blue;
+const Color mediumBlue = Color(0xff0651A5);
+const Color darkBlue = Color(0xff083A75);
+const Color grey = Color.fromARGB(255, 230, 229, 229);
+
+const Color baseBackground = Color(0xffF7F8FA); // Fundo Cinza Claro Base
+const Color whiteBackground = Color(0xffFFFFFF); // Fundo Branco
+const Color primaryBlue = Color(0xff083A75); // Azul Primário
+const Color highlightBlue = Color(0xff1168C8); // Azul de Destaque
+const Color iconColor = Color(0xff343A40); // Cor dos Ícones
+const Color headingColor = Color(0xff0E407B); // Cor do Título Principal
+const Color subheadingColor = Color(0xff343A40); // Cor do Subtítulo
+const Color secondaryTextColor = Color(0xff949CB2); // Cor do Texto Secundário
+const Color readOnlyTextColor = Color(0xffEDEFF5);
