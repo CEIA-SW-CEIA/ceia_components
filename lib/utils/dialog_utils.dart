28452be79ab5df1559fa963e90229e820fa73ca5
Francisco Gandala <gandala@discente.ufg.br>
2025-06-26 01:48:45 -0300

    fix:Atualização dos pop-ups

--- lib/utils/dialog_utils.dart
+++ lib/utils/dialog_utils.dart
@@ -1,4 +1,5 @@
-import 'package:ceia_components/widgets/ceia_button.dart';
+
+import 'package:ceia_components/widgets/ceia_button_new.dart';
 import 'package:ceia_components/widgets/ceia_text.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -6,95 +7,209 @@ import 'package:go_router/go_router.dart';
 class DialogUtils {
   DialogUtils._();
 
-  static Future showInformativeSuccessDialog(BuildContext context, String message) {
+  ///Constroi o pop-up de alerta, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário
+  static Future showInformativeAlertDialog(BuildContext context, String message) {
     return _showDialog(
       context,
-      'Sucesso!',
+      "ceia_components/image/ceia_alert_image.png",
+      'Alerta!',
       message,
-      [
-        CEIAButton.large(
-          onPressed: () => Navigator.pop(context),
-          text: 'Ok',
-          height: 45,
-        )
-      ],
+      _buildActions(null, context),
     );
   }
 
+  ///Constroi o pop-up de sucesso, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário
+  static Future showInformativeSuccessDialog(BuildContext context, String message) {
+    return _showDialog(
+      context,
+      "ceia_components/image/ceia_confirmation_image.png",
+      'Confirmação!',
+      message,
+      _buildActions(null, context),
+    );
+  }
+
+  ///Constroi o pop-up de erro, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário
   static Future showInformativeErrorDialog(BuildContext context, String message, {bool barrierDismissible = true}) {
     return _showDialog(
       context,
-      'Ops!',
+      "ceia_components/image/ceia_error_image.png",
+      'Erro!',
       message,
-      [
-        CEIAButton.large(
-          onPressed: () => Navigator.pop(context),
-          text: 'Ok',
-          height: 45,
-        )
-      ],
+      _buildActions(null, context),
       barrierDismissible: barrierDismissible,
     );
   }
 
-  static showConfirmDialog({
-    required BuildContext context,
-    required String title,
-    required String message,
-    required Function() onConfirm,
-  }) {
+  ///Constroi o pop-up de informação, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário
+  static Future showInformativeInformation(BuildContext context, String message, {bool barrierDismissible = true}) {
     return _showDialog(
       context,
-      title,
+      "ceia_components/image/ceia_info_image.png",
+      'Informação!',
       message,
-      [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            CEIAButton(
-              onPressed: () => Navigator.pop(context),
-              color: const Color(0xff949CB2),
-              text: 'Cancelar',
-              height: 45,
-            ),
-            const SizedBox(width: 16),
-            CEIAButton(
-              onPressed: () {
-                onConfirm();
-                context.pop();
-              },
-              text: 'Continuar',
-              height: 45,
-            ),
-          ],
-        )
-      ],
+      _buildActions(null, context),
+      barrierDismissible: barrierDismissible,
     );
   }
 
-  static Future _showDialog(BuildContext context, String title, String message, List<Widget>? actions,
+  ///Constroi o pop-up de dúvida, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário
+  static Future showInformativeDoubt(BuildContext context, String message, {bool barrierDismissible = true}) {
+    return _showDialog(
+      context,
+      "ceia_components/image/ceia_doubt_image.png",
+      'Dúvida',
+      message,
+      _buildActions(null, context),
+      barrierDismissible: barrierDismissible,
+    );
+  }
+
+  ///Constroi o pop-up de confirmação, o paramêtro [message] representa a mensagem que
+  ///será exibida para o usuário, o paramêtro [confirm] é a função que será executada
+  ///quando o usuário clica em confirmar
+  static Future showConfirmDialog(BuildContext context, String message, Function() confirm,
+      {bool barrierDismissible = true}) {
+    return _showDialog(
+      context,
+      "ceia_components/image/ceia_issue_image.png",
+      'Questão',
+      message,
+      _buildActions(confirm, context),
+      barrierDismissible: barrierDismissible,
+    );
+  }
+
+  //Constroi a estrutura do pop-up
+  static Future _showDialog(BuildContext context, String imagePath, String title, String message, Widget actions,
       {bool barrierDismissible = true}) {
     return showDialog(
       barrierDismissible: barrierDismissible,
       context: context,
-      builder: (context) => AlertDialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        title: Center(
-            child: Column(
-          children: [
-            Text(title),
-            const Divider(),
-          ],
-        )),
-        content: Container(
-          constraints: const BoxConstraints(maxWidth: 650, maxHeight: 500),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
-            child: CEIAText.bodyLarge(text: message),
+      builder: (context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SizedBox(
+          width: 320,
+          height: 174,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //Botão de fechar
+              _buildCloseButton(context),
+              //Sessão que contem a imagem, o título e o texto do pop-up
+              _buildImageAndText(imagePath, title, message),
+              const SizedBox(
+                height: 16,
+              ),
+              //Botões presentes na parte inferior do pop-up
+              actions
+            ],
           ),
         ),
-        actions: actions,
       ),
+    );
+  }
+
+  //Constroi o conteúdo principal de pop-up
+  static Widget _buildImageAndText(String imagePath, String title, String message) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          width: 24,
+        ),
+        Image.asset(
+          imagePath,
+          width: 58,
+          height: 58,
+        ),
+        const SizedBox(
+          width: 17,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CEIAText.titleSmall(text: title),
+            const SizedBox(
+              height: 8,
+            ),
+            CEIAText.bodyLarge(
+              text: message,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  static Widget _buildCloseButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          onPressed: () {
+            context.pop();
+          },
+          icon: const Icon(
+            Icons.close,
+            size: 14,
+          ),
+        )
+      ],
+    );
+  }
+
+  //Constroi os botões que estão presentes na parte inferior do pop-up
+  static Widget _buildActions(Function()? action, BuildContext context) {
+    if (action != null) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 54),
+        child: Row(
+          children: [
+            CeiaButtonNew(
+              borderColor: Colors.grey,
+              textColor: Colors.grey,
+              borderRadius: 5,
+              color: Colors.white,
+              onPressed: () {
+                context.pop();
+              },
+              text: 'Não',
+              width: 97,
+              height: 26,
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            CeiaButtonNew(
+              borderRadius: 5,
+              text: 'Sim',
+              width: 97,
+              height: 26,
+              onPressed: () {
+                action.call();
+                context.pop();
+              },
+            ),
+          ],
+        ),
+      );
+    }
+    return CeiaButtonNew(
+      borderRadius: 5,
+      text: 'Fechar',
+      width: 97,
+      height: 26,
+      onPressed: () {
+        context.pop();
+      },
     );
   }
 
